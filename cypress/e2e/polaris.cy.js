84662de5dd5b874c78fe745446d760ed8348ab85
Vijay <vijaytest5555@gmail.com>
2024-11-07 16:54:56 +0530

    up[dated testcases]

--- cypress/e2e/polaris.cy.js
+++ cypress/e2e/polaris.cy.js
@@ -1,7 +1,8 @@
 describe("Polaris", () => {
 
     beforeEach ("set viewport", ()=> {
-    cy.viewport(1920,1080);
+    
+    cy.viewport(1600, 747);
     cy.visit("https://polaris-staging-evp.eloci.us/");
     cy.get('#outlined-basic').type("vijaytest5555+post01@gmail.com");
     cy.get('#password').type("Zoro@test123");
@@ -11,14 +12,28 @@ describe("Polaris", () => {
     });
     
     it("verify title of the page", () => {
-    cy.title().should('eq','Elo')
-    });
-
-    it("verify user able to add device", () => {
-        cy.get('[data-cy="deviceComponent-module-addDeviceButton"] > .MuiButton-label').click();
-        cy.get(':nth-child(1) > :nth-child(1) > .textfieddeactv > [data-cy="addDevice-input-name"] > .MuiInputBase-root > #outlined-basic').click().type('autoDevice');
-        cy.get(':nth-child(1) > :nth-child(2) > .textfieddeactv > [data-cy="addDevice-input-serial"] > .MuiInputBase-root > #outlined-basic').click().type('G98774773');
-        cy.get('.devicedialogright > .buttoncomponent-start > .MuiButtonBase-root').click();
-
+        cy.title().should('eq','Elo')
         });
-});
+
+    it("Check the number row and cloumns in device page", () => {
+        cy.title().should('eq','Elo')
+        });
+
+    it('verify user can add a device if the device is not exist already', () => {
+        if (
+            cy.xpath("//tbody//tr//td").should('be.visible')
+            .should('not.have.text', 'autoDevice'))
+            {
+                // Add a new device
+            cy.get('[data-cy="deviceComponent-module-addDeviceButton"]').click();
+            cy.get(':nth-child(1) > :nth-child(1) > .textfieddeactv > [data-cy="addDevice-input-name"] > .MuiInputBase-root > #outlined-basic').type('autoDevice');
+            cy.get(':nth-child(1) > :nth-child(2) > .textfieddeactv > [data-cy="addDevice-input-serial"] > .MuiInputBase-root > #outlined-basic').type('G98774773');
+            cy.get('.devicedialogright .buttoncomponent-start .MuiButtonBase-root').click();
+            }
+                        
+        else {
+            // Add a new device
+            console.log("delete device");
+             }
+      });
+    });
